ab96be6064597382b917465d474d79cc623f54dc
Wendell Mutia <89864599+JonDellX@users.noreply.github.com>
2023-01-27 21:50:44 -0500

    Established directories for project; created http server for req,res

---
+++ first web game/Javascript/server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+response.writeHead(200, {
+'Content-Type': 'text/plain',
+'Access-Control-Allow-Origin' : '*'
+});
+
+response.end('Hello World\n');
+}).listen(1337);
+
+console.log("Server running on port 1337!");
